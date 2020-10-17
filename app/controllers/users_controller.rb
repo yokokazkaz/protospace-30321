4950368e4f6888f5750dc6e6b44f6d8a636131dc
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-10-17 13:11:50 +0900

    first_commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @name = @user.name
+    @prototypes = @user.prototypes
+    @profile = @user.profile
+    @occupation = @user.occupation
+    @position = @user.position
+  end
+
+end
